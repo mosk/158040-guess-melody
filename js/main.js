5be7a40d52fb60ba25871ec4f9276a0fb333832c
Mihail <miiixsc@gmail.com>
2018-05-31 18:25:49 +0300

    добавил кнопки и обработку кликов

--- js/main.js
+++ js/main.js
@@ -1,7 +1,35 @@
 'use strict';
 
-const container = document.querySelector('.app section.main');
+const KEYCODE = {
+  left: 37,
+  right: 39
+};
+
+const app = document.querySelector('.app');
+const container = app.querySelector('section.main');
 const templates = document.querySelector('#templates');
+const templateArrows = `<div class="arrows__wrap">
+                  <style>
+                    .arrows__wrap {
+                      position: absolute;
+                      top: 135px;
+                      left: 50%;
+                      margin-left: -56px;
+                    }
+                    .arrows__btn {
+                      background: none;
+                      border: 2px solid black;
+                      padding: 5px 20px;
+                    }
+                  </style>
+                  <button class="arrows__btn arrows__btn--prev"><-</button>
+                  <button class="arrows__btn arrows__btn--next">-></button>
+                </div>`
+
+app.insertAdjacentHTML('beforeend', templateArrows);
+
+const buttonPrev = app.querySelector('.arrows__btn--prev');
+const buttonNext = app.querySelector('.arrows__btn--next');
 
 const Screen = {
   welcome: templates.content.querySelector('.main--welcome'),
@@ -18,24 +46,49 @@ for (const key in Screen) {
   screens.push(Screen[key]);
 };
 
-const showScreen = (number) => container.appendChild((screens[number]).cloneNode(true));
+let currentScreen = 0;
 
-const Keycode = {
-  left: 37,
-  right: 39
+const showScreen = (arg) => { // можно указать конкретный номер экрана либо следующий или предыдущий экран
+  container.innerHTML = ``;
+
+  if (typeof arg === Number && arg >= 0 && arg < screens.length) {
+    container.appendChild((screens[arg]).cloneNode(true));
+    currentScreen = arg;
+    console.log(`Номер экрана - ${currentScreen}`);
+
+    return;
+  }
+
+  switch (arg) {
+    case 'next':
+      container.appendChild((screens[currentScreen + 1]).cloneNode(true));
+      currentScreen++;
+      console.log(`Номер экрана - ${currentScreen}`);
+      break;
+    case 'prev':
+      container.appendChild((screens[currentScreen - 1]).cloneNode(true));
+      currentScreen--;
+      console.log(`Номер экрана - ${currentScreen}`);
+      break;
+  }
 };
 
 document.addEventListener('keydown', (e) => {
   switch (e.keyCode) {
-    case Keycode.left:
+    case KEYCODE.left:
       console.log('кнопка влево');
-      showPrevScreen();
+      showScreen('prev');
       break;
-    case Keycode.right:
+    case KEYCODE.right:
       console.log('кнопка вправо');
-      showNextScreen();
+      showScreen('next');
       break;
   }
 });
 
-showScreen(0);
+showScreen(currentScreen);
+
+buttonPrev.addEventListener('click', () => showScreen('prev'));
+buttonNext.addEventListener('click', () => showScreen('next'));
+
+console.log(`Номер экрана - ${currentScreen}`);
