ad6b3e27ffece0119b68005eebef6fbf117ba20d
Mihail <miiixsc@gmail.com>
2018-06-01 14:21:49 +0300

    *

--- js/main.js
+++ js/main.js
@@ -5,9 +5,9 @@ const Keycode = {
   RIGHT: 39
 };
 
-const app = document.querySelector('.app');
-const container = app.querySelector('section.main');
-const templates = document.querySelector('#templates');
+const app = document.querySelector(`.app`);
+const container = app.querySelector(`section.main`);
+const templates = document.querySelector(`#templates`);
 const templateArrows = `<div class="arrows__wrap">
   <style>
     .arrows__wrap {
@@ -24,57 +24,55 @@ const templateArrows = `<div class="arrows__wrap">
   </style>
   <button class="arrows__btn arrows__btn--prev"><-</button>
   <button class="arrows__btn arrows__btn--next">-></button>
-</div>`
+</div>`;
 
-app.insertAdjacentHTML('beforeend', templateArrows);
+app.insertAdjacentHTML(`beforeend`, templateArrows);
 
-const buttonPrev = app.querySelector('.arrows__btn--prev');
-const buttonNext = app.querySelector('.arrows__btn--next');
+const buttonPrev = app.querySelector(`.arrows__btn--prev`);
+const buttonNext = app.querySelector(`.arrows__btn--next`);
 
 const Screen = {
-  WELCOME: templates.content.querySelector('.main--welcome'),
-  GENRE: templates.content.querySelector('.main--level-genre'),
-  ARTIST: templates.content.querySelector('.main--level-artist'),
-  RESULT: templates.content.querySelector('.main--result'),
-  ERROR: templates.content.querySelector('.modal-error'),
-  CONFIRM: templates.content.querySelector('.modal-confirm__wrap')
+  WELCOME: templates.content.querySelector(`.main--welcome`),
+  GENRE: templates.content.querySelector(`.main--level-genre`),
+  ARTIST: templates.content.querySelector(`.main--level-artist`),
+  RESULT: templates.content.querySelector(`.main--result`),
+  ERROR: templates.content.querySelector(`.modal-error`),
+  CONFIRM: templates.content.querySelector(`.modal-confirm__wrap`)
 };
 
 const screens = [];
 
 for (const key in Screen) {
-  screens.push(Screen[key]);
-};
+  if (key) {
+    screens.push(Screen[key]);
+  }
+}
 
 let currentScreen = 0;
 
 const showScreen = (arg) => { // можно указать конкретный номер экрана либо следующий или предыдущий экран
-  container.innerHTML = ``;
-
   if (typeof arg === `number` && arg >= 0 && arg < screens.length) {
+    container.innerHTML = ``;
     container.appendChild((screens[arg]).cloneNode(true));
     currentScreen = arg;
 
     return;
-  }
+  } else if (arg === `next` && currentScreen !== screens.length - 1) {
+    container.innerHTML = ``;
+    container.appendChild((screens[currentScreen + 1]).cloneNode(true));
+    currentScreen++;
 
-  switch (arg) {
-    case 'next':
-      if (currentScreen === screens.length - 1) return;
+    return;
+  } else if (arg === `prev` && currentScreen !== 0) {
+    container.innerHTML = ``;
+    container.appendChild((screens[currentScreen - 1]).cloneNode(true));
+    currentScreen--;
 
-      container.appendChild((screens[currentScreen + 1]).cloneNode(true));
-      currentScreen++;
-      break;
-    case 'prev':
-      if (currentScreen === 0) return;
-
-      container.appendChild((screens[currentScreen - 1]).cloneNode(true));
-      currentScreen--;
-      break;
+    return;
   }
 };
 
-document.addEventListener('keydown', (e) => {
+document.addEventListener(`keydown`, (e) => {
   switch (e.keyCode) {
     case Keycode.LEFT:
       showScreen(`prev`);
@@ -87,5 +85,5 @@ document.addEventListener('keydown', (e) => {
 
 showScreen(currentScreen);
 
-buttonPrev.addEventListener('click', () => showScreen(`prev`));
-buttonNext.addEventListener('click', () => showScreen(`next`));
+buttonPrev.addEventListener(`click`, () => showScreen(`prev`));
+buttonNext.addEventListener(`click`, () => showScreen(`next`));
